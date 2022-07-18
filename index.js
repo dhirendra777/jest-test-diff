c5fd3997cd5f58dd31b1af1766d36d3e544433e9
Dhirendra <dhirendra.naik@tangorxsolutions.com>
2022-07-19 01:03:20 +0530

    update code to read file

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
+const { promises: fs } = require("fs");
+const exec = require("@actions/exec");
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  //   const nameToGreet = core.getInput('who-to-greet');
+  //   console.log(`Hello ${nameToGreet}!`);
+  //   const time = (new Date()).toTimeString();
+  //   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  //   const payload = JSON.stringify(github.context.payload, undefined, 2)
+  //   console.log(`The event payload: ${payload}`);
+
+  const content = exec.exec(
+    `git show origin/${github.event.pull_request.base.ref}`
+  );
+  const path = core.getInput("path");
+  const trim = core.getBooleanInput("trim");
+  //   let content = await fs.readFile(path, "utf8");
+  //   if (trim) {
+  //     content = content.trim();
+  //   }
+
+  core.setOutput("content", content, JSON.parse(content));
 } catch (error) {
   core.setFailed(error.message);
 }
