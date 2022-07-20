cb56d6c0a8b7c00e34bfd0afe68b9e04d7d32d72
Dhirendra <dhirendra.naik@tangorxsolutions.com>
2022-07-20 11:40:58 +0530

    update code to read file

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ const main = async () => {
   );
 
   /* Read file in memory and compare package.json */
-  let baseContent = await fs.readFile("~/base.json", "utf8");
-  let headContent = await fs.readFile("~/head.json", "utf8");
+  let baseContent = await fs.readFile("base.json", "utf8");
+  let headContent = await fs.readFile("head.json", "utf8");
 
   console.log("Base content is ", baseContent);
   console.log("Head content is ", headContent);
