9113f276d73beb7064a5c7fc55549f2f7ad44bf4
Dhirendra <dhirendra.naik@tangorxsolutions.com>
2022-07-20 10:50:42 +0530

    update code to read file

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const exec = require("@actions/exec");
 const main = async () => {
   /* Dump in file */
   await exec.exec(
-    `git show origin/${github.context.payload.pull_request.base.ref}:./package.json > base.json`
+    `git show origin/${github.context.payload.pull_request.base.ref}:./package.json > ~/base.json`
   );
 
   await exec.exec(
-    `git" show origin/${github.context.payload.pull_request.head.ref}:./package.json > head.json`
+    `git" show origin/${github.context.payload.pull_request.head.ref}:./package.json > ~/head.json`
   );
 
   /* Read file in memory and compare package.json */
-  let baseContent = await fs.readFile("./base.json", "utf8");
-  let headContent = await fs.readFile("./head.json", "utf8");
+  let baseContent = await fs.readFile("~/base.json", "utf8");
+  let headContent = await fs.readFile("~/head.json", "utf8");
 
   console.log("Base content is ", baseContent);
   console.log("Head content is ", headContent);
